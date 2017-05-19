2bdf8ed77b9f706f0502ab6fc2b4a489e4257baa
Nadahar <Nadahar@users.noreply.github.com>
2017-05-19 14:15:45 +0200

    Implemented macOS CoreFoundation Framework JNA mappings

---
+++ src/main/java/net/pms/util/jna/macos/corefoundation/TerminatedStringEncodingArray.java
@@ -0,0 +1,53 @@
+/*
+ * Universal Media Server, for streaming any media to DLNA compatible renderers
+ * based on the http://www.ps3mediaserver.org. Copyright (C) 2012 UMS
+ * developers.
+ *
+ * This program is a free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2 of the License only.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package net.pms.util.jna.macos.corefoundation;
+
+import java.util.Collection;
+import com.sun.jna.Pointer;
+import net.pms.util.jna.TerminatedArray;
+import net.pms.util.jna.TerminatedIntArray;
+import net.pms.util.jna.macos.corefoundation.CoreFoundation.CFStringBuiltInEncodings;
+
+/**
+ * A representation of an array of {@code CFStringEncoding} terminated by
+ * {@link CFStringBuiltInEncodings#kCFStringEncodingInvalidId}.
+ *
+ * @see TerminatedArray
+ *
+ * @author Nadahar
+ */
+@SuppressWarnings("checkstyle:JavadocMethod")
+public class TerminatedStringEncodingArray extends TerminatedIntArray {
+
+	public TerminatedStringEncodingArray() {
+	}
+
+	public TerminatedStringEncodingArray(Pointer p) {
+		super(p);
+	}
+
+	public TerminatedStringEncodingArray(Collection<? extends Integer> source) {
+		super(source);
+	}
+
+	@Override
+	public Integer getTerminator() {
+		return CFStringBuiltInEncodings.kCFStringEncodingInvalidId.getValue();
+	}
+}
